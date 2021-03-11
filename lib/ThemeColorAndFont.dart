805dc83ca5946c5c1a5c32949deeef3cf653f21b
coenen <coenen@aliyun.com>
2021-03-11 17:40:09 +0800

    使用主题共享颜色和字体样式

---
+++ lib/ThemeColorAndFont.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    final appName = 'Custom Themes';
+
+    return MaterialApp(
+      title: appName,
+      theme: new ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.orange,
+        accentColor: Colors.deepPurpleAccent,
+      ),
+      home: new MyHomePage(
+        title: appName,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  final String title;
+  MyHomePage({Key key,@required this.title}): super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Container(
+          color: Theme.of(context).accentColor,
+          child: new Text('Text with a background color',
+          style: Theme.of(context).textTheme.title,),
+        ),
+      ),
+      floatingActionButton: new Theme(
+          data: Theme.of(context).copyWith(accentColor: Colors.red), 
+          child: new FloatingActionButton(
+              onPressed: null,
+          child: new Icon(Icons.add),)),
+    );
+  }
+}
