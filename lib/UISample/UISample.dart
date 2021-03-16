8a98b7ca790b2b2e96912ad46b690e7ee0e5f6cf
coenen <coenen@aliyun.com>
2021-03-16 11:12:17 +0800

    排列说明

--- lib/UISample/UISample.dart
+++ lib/UISample/UISample.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'UIAdapt.dart';
@@ -7,102 +6,137 @@ class UISampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TitleSection
-   Widget titleSection = new Container(
-     padding: const EdgeInsets.all(32),
-     child: new Row(
-       children: [
-         new Expanded(child: new Column(
-           crossAxisAlignment: CrossAxisAlignment.start,
-           children: [
-             new Container(
-               padding: EdgeInsets.only(bottom: 8),
-               child: new Text(
-                 'Oeschinen Lake Campground',
-                 style: new TextStyle(
-                   fontWeight: FontWeight.bold
-                 ),
-               ),
-             ),
-             new Text(
-               'Kandersteg, Switzerland',
-               style: new TextStyle(
-                 color: Colors.grey[500],
-               ),
-             ),
-           ],
-         )),
-         new Icon(Icons.star,
-         color: Colors.red[500],
-         ),
-         new Text('41'),
-       ],
-     ),
-   );
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32),
+      child: new Row(
+        children: [
+          new Expanded(
+              child: new Column(
+                /* enum CrossAxisAlignment {
+                    //将子控件放在交叉轴的起始位置
+                    start,
 
-   Column buildButtonColumn(IconData icon, String label){
-     Color color = Theme.of(context).primaryColor;
+                    //将子控件放在交叉轴的结束位置
+                    end,
 
-     return new Column(
-       mainAxisAlignment: MainAxisAlignment.center,
-       mainAxisSize: MainAxisSize.min,
-       children: [
-         new Icon(icon,color: color,),
-         new Container(
-           margin: const EdgeInsets.only(top:8.0),
-           child: new Text(
-             label,
-             style: new TextStyle(
-               fontSize: 12,
-               fontWeight: FontWeight.w400,
-               color: color,
-             ),
-           ),
-         )
-       ],
-     );
-   }
+                    //将子控件放在交叉轴的中间位置
+                    center,
 
-Widget buttonSection = new Container(
-  child: new Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      buildButtonColumn(Icons.call, 'CALL'),
-      buildButtonColumn(Icons.near_me, 'ROUTE'),
-      buildButtonColumn(Icons.share, 'SHARE'),
-    ],
-  ),
-);
+                    //使子控件填满交叉轴
+                    stretch,
 
-   Widget textSection = new Container(
-     padding: const EdgeInsets.all(32),
-     child: new Text(
-       '''
+                    //将子控件放在交叉轴的上，并且与基线相匹配（不常用）
+                    baseline,
+                    }*/
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              new Container(
+                padding: EdgeInsets.only(bottom: 8),
+                child: new Text(
+                  'Oeschinen Lake Campground',
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              new Text(
+                'Kandersteg, Switzerland',
+                style: new TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          )),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text('41'),
+        ],
+      ),
+    );
+
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return new Column(
+        /**enum MainAxisAlignment {
+            //将子控件放在主轴的开始位置
+            start,
+            //将子控件放在主轴的结束位置
+            end,
+            //将子控件放在主轴的中间位置
+            center,
+            //将主轴空白位置进行均分，排列子元素，手尾没有空隙
+            spaceBetween,
+            //将主轴空白区域均分，使中间各个子控件间距相等，首尾子控件间距为中间子控件间距的一半
+            spaceAround,
+            //将主轴空白区域均分，使各个子控件间距相等
+            spaceEvenly,
+            }*/
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          new Icon(
+            icon,
+            color: color,
+          ),
+          new Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = new Container(
+      padding: const EdgeInsets.all(32),
+      child: new Text(
+        '''
 Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
        ''',
-       softWrap: true,
-     ),
-   );
+        softWrap: true,
+      ),
+    );
 
-   return new MaterialApp(
-     title: 'Flutter Demo',
-     theme: new ThemeData(
-       primarySwatch: Colors.blue,
-     ),
-     home: new Scaffold(
-       appBar: new AppBar(
-         title: new Text('Flutter Demo'),
-       ),
-       body: new ListView(
-         children: [
-           new Image.asset('Image/11.jpeg',
-             height: 240,
-             fit: BoxFit.cover,),
-           titleSection,
-           buttonSection,
-           textSection,
-         ],
-       ),
-     ),
-   );
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter Demo'),
+        ),
+        body: new ListView(
+          children: [
+            new Image.asset(
+              'Image/11.jpeg',
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
   }
 }
