69c970a629486dafa69b96d6c157cabbb6ec1656
coenen <coenen@aliyun.com>
2021-03-17 10:08:58 +0800

    Card

--- lib/UISample/OftenUI.dart
+++ lib/UISample/OftenUI.dart
@@ -7,7 +7,7 @@ class OftenUIApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: '常用控件',
-      home: new StackVC(),
+      home: new CardVC(),
     );
   }
 }
@@ -166,3 +166,56 @@ class StackVC extends StatelessWidget{
   }
 }
 
+
+class CardVC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    var card = new SizedBox(
+      height: 210,
+      child: new Card(
+        child: new Column(
+          children: [
+            new ListTile(
+              title: new Text('1625 主街道',
+              style: new TextStyle(
+                fontWeight: FontWeight.w500
+              ),),
+              subtitle: new Text('加利福尼亚 99984'),
+              leading: new Icon(Icons.restaurant_menu,
+              color: Colors.blue[500],),
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text('(408) 55-1222',
+              style: new TextStyle(
+                fontWeight: FontWeight.w500
+              ),),
+              leading: new Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            new ListTile(
+              title: new Text('costa@example.com'),
+              leading: new Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
+    return new MaterialApp(
+      title: 'Card Demo',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Card Demo'),
+        ),
+        body: card,
+      ),
+    );
+  }
+}
