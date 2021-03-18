28843bd1af71e6faefe482000b072770eb95ace2
coenen <coenen@aliyun.com>
2021-03-18 09:28:57 +0800

    添加和删除视图

--- lib/FlutterForIOS.dart
+++ lib/FlutterForIOS.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class FluterIOSAPP extends StatelessWidget{
   @override
@@ -7,18 +8,18 @@ class FluterIOSAPP extends StatelessWidget{
     return MaterialApp(
       title: 'Flutter For IOS',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: FlutterForIOSVC(),
+      home: WidgetManager(),
     );
   }
 }
 
-class FlutterForIOSVC extends StatefulWidget{
-  FlutterForIOSVC({Key key}) : super(key: key);
+class UpdateWidgetVC extends StatefulWidget{
+  UpdateWidgetVC({Key key}) : super(key: key);
 
-  _FlutterForIOSVCState createState() => new _FlutterForIOSVCState();
+  _UpdateWidgetVCState createState() => new _UpdateWidgetVCState();
 }
 
-class _FlutterForIOSVCState extends State<FlutterForIOSVC> {
+class _UpdateWidgetVCState extends State<UpdateWidgetVC> {
   String textToShow = 'I Like Flutter';
 int count = 1;
   void request() {
@@ -32,14 +33,62 @@ int count = 1;
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Flutter For IOS'),
+        title: new Text('Update Widget Data'),
       ),
       body: Center(
-        child: Text(textToShow),
+        child: CupertinoButton(
+          onPressed: (){
+
+          },
+          child: Text('hello'),
+          color: Colors.yellow,
+          padding: EdgeInsets.only(left: 40,right: 10),
+        ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: request,
       tooltip: 'Update Data',
       child: new Icon(Icons.update),),
+    );
+  }
+}
+
+class WidgetManager extends StatefulWidget {
+  WidgetManager({Key key}): super(key: key);
+  _WidgetManagerState createState() => new _WidgetManagerState();
+}
+
+class _WidgetManagerState extends State<WidgetManager>{
+  bool toggle = true;
+  void _toggle() {
+    setState(() {
+      toggle = !toggle;
+    });
+  }
+
+  _getToggleChild() {
+    if (toggle) {
+      return Text('Toggle One');
+    }else {
+      return CupertinoButton(child: Text('Toggle Two'), onPressed: (){
+        print("第二个来了");
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('添加和删除视图'),
+      ),
+      body: Center(
+        child: _getToggleChild(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggle,
+        tooltip: 'Update Text',
+        child: Icon(Icons.update),
+      ),
     );
   }
 }
